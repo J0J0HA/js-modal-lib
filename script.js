d9c616eb65c6abb13d59126d3f815d9109d6d062
JoJoJux aka. J0J0HA <info@jojojux.de>
2023-05-10 20:47:35 +0000

    Chaged example code

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 window.onload = (e) => {
-    let dia = new JSModalDialog("Test Dialog");
-
-    let testelm = document.createElement("img");
-    testelm.src="https://picsum.photos/100/100"
-    dia.addElement(testelm);
-    dia.apply();
-    dia.show();
+    let dia = new JSModalDialog(JSModalType.YN, "Test Dialog");
+    dia.addText("Should we do this?");
+    dia.show()
+        .then(alert)
+        .catch(console.error);
+    dia = new JSModalDialog(JSModalType.Sure, "Test Dialog");
+    dia.addText("We will do this.");
+    dia.show()
+        .then(alert)
+        .catch(console.error);
+    dia = new JSModalDialog(JSModalType.Info, "Test Dialog");
+    dia.addText("It was done.");
+    dia.show()
+        .then(alert)
+        .catch(console.error);
 }
